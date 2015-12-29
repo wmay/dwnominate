5d410e98430b71a97841b068123c2215801154da
William May <williamcmay@live.com>
2015-12-29 15:47:05 -0600

    added readKH compatibility and fixed some bugs

--- R/dwnominate.R
+++ R/dwnominate.R
@@ -2,41 +2,54 @@
 
 # limitations:
 
-# 600 legislators / session
+# 600 legislators per session
 
-# 3600 votes / session
+# 3600 votes per session
+
+format_column = function(df, name, format, alternative) {
+  if (name %in% names(df)) {
+    x = df[, name]
+  } else {
+    x = alternative
+  }
+  sprintf(format, x)
+}
 
 write_rc_data_file = function(rc_list) {
   all_lines = vector()
-  session = 1
-  for (n in 1:length(rc_list)) {
-    rc = rc_list[[n]]
+  for (session in 1:length(rc_list)) {
+    rc = rc_list[[session]]
+    rcl = rc$legis.data
     votes = rc$votes
-    sessions = sprintf("%4.0f", session)
-    leg_ids = sprintf("%5.0f", rc$legis.data$ID)
-    state_num = sprintf("%2.0f", 1)
-    district = sprintf("%1.0f", 0)
-    state_name = sprintf("%-7s", "Vermont")
-    parties = sprintf("%4d", rc$legis.data[["party"]])
+    codes = rc$codes
+    sessions =
+      format_column(rcl, "sessionID", "%4d", session)
+    leg_ids =
+      format_column(rcl, "icpsrLegis", "%6d", rcl$ID)
+    state_num =
+      format_column(rcl, "icpsrState", "%3d", 0)
+    district =
+      format_column(rcl, "cd", "%2d", 0)
+    state_name =
+      format_column(rcl, "state", "%-7s", "NA")
+    parties =
+      format_column(rcl, "party", "%4d", 0)
     leg_names = sprintf("%-11.11s",
-        gsub("[^A-Za-z -]", "", rownames(rc$votes)))
+        gsub("[^A-Za-z ()'-]", "", rownames(votes)))
     
-    # a dictionary to translate votes into the appropriate numbers
-    # required by DW-NOMINATE
-    vote_dict = setNames(c(1, 6, 9, 0), c("yes", "no", "other", "NA"))
-    votes[is.na(votes)] = "NA"
-    # instead of doing that vote dictionary, replace text with nums
-    # directly:
-
+    votes[votes %in% codes$yea] = 1
+    votes[votes %in% codes$nay] = 6
+    votes[votes %in% c(codes$notInLegis, codes$missing)] = 9
+    
     # a vector of very long lists of numbers, one for each legislator
-    vote_nums = apply(votes, 1, FUN = function(x) paste(vote_dict[x],
-                                    collapse = ""))
+    vote_nums =
+      apply(votes, 1, FUN = function(x) paste(x, collapse = ""))
 
     # the lines to be written to the file
-    lines = paste(sessions, leg_ids, state_num, district, state_name,
-        parties, paste0(leg_names, vote_nums))
+    "I4,I6,I3,I2,1X,7A1,1X,I4,1X,11A1,3600I1"
+    lines = paste0(sessions, leg_ids, state_num, district, " ",
+        state_name, " ", parties, " ", leg_names, vote_nums)
     all_lines = c(all_lines,  lines)
-    session = session + 1
   }
   
   writeLines(all_lines, "rollcall_matrix.vt3")
@@ -44,28 +57,24 @@ write_rc_data_file = function(rc_list) {
 
 write_transposed_rc_data_file = function(rc_list) {
   all_lines = vector()
-  session = 1
-  for (n in 1:length(rc_list)) {
-    rc = rc_list[[n]]
-    votes = rc$votes
-    vote_ids = sprintf("%4.0f", 1:ncol(rc$votes))
-    sessions = sprintf("%4.0f", session)
+  for (session in 1:length(rc_list)) {
+    votes = rc_list[[session]]$votes
+    codes = rc_list[[session]]$codes
+    sessions = sprintf("%4d", session)
+    vote_ids = sprintf("%5d", 1:ncol(votes))
     
-    # a dictionary to translate votes into the appropriate numbers
-    # required by DW-NOMINATE
-    vote_dict = setNames(c(1, 6, 9, 0), c("yes", "no", "other", "NA"))
-    votes[is.na(votes)] = "NA"
-    # instead of doing that vote dictionary, replace text with nums
-    # directly:
+    votes[votes %in% codes$yea] = 1
+    votes[votes %in% codes$nay] = 6
+    votes[votes %in% c(codes$notInLegis, codes$missing)] = 9
 
     # a vector of very long lists of numbers, one for each legislator
-    vote_nums = apply(votes, 2, FUN = function(x) paste0(vote_dict[x],
-                                    collapse = ""))
+    vote_nums =
+      apply(votes, 2, FUN = function(x) paste(x, collapse = ""))
 
     # the lines to be written to the file
-    lines = paste(sessions, vote_ids, vote_nums)
+    "I4,I5,1X,600I1"
+    lines = paste0(sessions, vote_ids, " ", vote_nums)
     all_lines = c(all_lines, lines)
-    session = session + 1
   }
   writeLines(all_lines, "transposed_rollcall_matrix.vt3")
 }
@@ -74,29 +83,40 @@ write_leg_file = function(rc_list, wnom_list) {
   all_lines = vector()
   for (session in 1:length(rc_list)) {
     rc = rc_list[[session]]
+    rcl = rc$legis.data
     wnom = wnom_list[[session]]
-    sessions = sprintf("%4d", session)
-    leg_ids = sprintf("%5d", rc$legis.data$ID)
-    state_num = sprintf("%2d", 1)
-    district = sprintf("%1d", 0)
-    state_name = sprintf("%-7s", "Vermont")
-    parties = sprintf("%4d", rc$legis.data[["party"]])
+    "I4,I6,I3,I2,1X,7A1,1X,I4,1X,11A1,2F7.3,I5"
+    sessions =
+      format_column(rcl, "sessionID", "%4d", session)
+    leg_ids =
+      format_column(rcl, "icpsrLegis", "%6d", rcl$ID)
+    state_num =
+      format_column(rcl, "icpsrState", "%3d", 0)
+    district =
+      format_column(rcl, "cd", "%2d", 0)
+    state_name =
+      format_column(rcl, "state", "%-7s", "NA")
+    parties =
+      format_column(rcl, "party", "%4d", 0)
     leg_names = sprintf("%-11.11s",
-        gsub("[^A-Za-z -]", "", rownames(rc$votes)))
+        gsub("[^A-Za-z ()'-]", "", rownames(rc$votes)))
     # DW-NOMINATE does not like NA's
     wnom$legislators[is.na(wnom$legislators)] = 0
-    coord1 = sprintf("%6.3f", wnom$legislators$coord1D)
-    coord2 = sprintf("%6.3f", wnom$legislators$coord2D)
-    votes = sprintf("%4d", rowSums(wnom$legislators[, 2:5]))
-    errors = sprintf("%4d",
-        rowSums(wnom$legislators[, c("wrongYea", "wrongNay")]))
-    gmp = sprintf("%6.3f", wnom$legislators$GMP)
+    coord1 = sprintf("%7.3f", wnom$legislators$coord1D)
+    coord2 = sprintf("%7.3f", wnom$legislators$coord2D)
+    all_votes = c("correctYea", "wrongYea", "wrongNay",
+        "correctNay")
+    ## votes = sprintf("%4d", rowSums(wnom$legislators[, all_votes]))
+    ## errors = sprintf("%4d",
+    ##     rowSums(wnom$legislators[, c("wrongYea", "wrongNay")]))
+    ## gmp = sprintf("%6.3f", wnom$legislators$GMP)
 
     # the lines to be written to the file
-    lines = paste(sessions, leg_ids, state_num, district, state_name,
-        parties, leg_names, coord1, coord2,
-        " 0.000  0.000  0.000  0.000     0.00000     0.00000",
-        votes, votes, errors, errors, gmp, gmp)
+    "I4,I6,I3,I2,1X,7A1,1X,I4,1X,11A1,2F7.3,I5"
+    lines = paste0(sessions, leg_ids, state_num, district, " ",
+        state_name, " ", parties, " ", leg_names, coord1, coord2)#,
+        ## " 0.000  0.000  0.000  0.000     0.00000     0.00000",
+        ## votes, votes, errors, errors, gmp, gmp)
     all_lines = c(all_lines, lines)
   }
   writeLines(all_lines, "legislator_input.dat")
@@ -107,17 +127,18 @@ write_bill_file = function(rc_list, wnom_list) {
   for (session in 1:length(rc_list)) {
     rc = rc_list[[session]]
     wnom = wnom_list[[session]]
+    "I3,I5,4F7.3"
     sessions = sprintf("%3d", session)
-    bill_ids = sprintf("%4d", 1:ncol(rc$votes))
+    bill_ids = sprintf("%5d", 1:ncol(rc$votes))
     # DW-NOMINATE does not like NA's
     wnom$rollcalls[is.na(wnom$rollcalls)] = 0
-    spread1 = sprintf("%6.3f", wnom$rollcalls$spread1D)
-    spread2 = sprintf("%6.3f", wnom$rollcalls$spread2D)
-    mid1 = sprintf("%6.3f", wnom$rollcalls$midpoint1D)
-    mid2 = sprintf("%6.3f", wnom$rollcalls$midpoint2D)
+    spread1 = sprintf("%7.3f", wnom$rollcalls$spread1D)
+    spread2 = sprintf("%7.3f", wnom$rollcalls$spread2D)
+    mid1 = sprintf("%7.3f", wnom$rollcalls$midpoint1D)
+    mid2 = sprintf("%7.3f", wnom$rollcalls$midpoint2D)
 
     # the lines to be written to the file
-    lines = paste(sessions, bill_ids, spread1, mid1, spread2, mid2)
+    lines = paste0(sessions, bill_ids, spread1, mid1, spread2, mid2)
     all_lines = c(all_lines, lines)
   }
   writeLines(all_lines, "rollcall_input.dat")
@@ -163,40 +184,37 @@ write_input_files = function(rc_list, wnom_list) {
 }
 
 read_output_files = function(party_dict) {
-  legwidths = c(4, 6, 3, 2, 8, 5, 12, 7, 7,
-      7, 7, 7, 7, 12, 12, 5, 5, 5, 5, 7, 7)
-  # column names based on info found here:
-  # http://voteview.com/rohde.htm
-  legnames = c("session", "ID", "stateID", "districtID",
+  # column names based on DW-NOMINATE fortran code and info found
+  # here: http://voteview.com/rohde.htm
+  nas = c("**", "***", "****", "*****", "******",
+      "*******", "************")
+  legnames = c("session", "ID", "stateID", "district",
       "state", "partyID", "name", "coord1D", "coord2D",
       "se1D", "se2D", "var1D", "var2D", "loglikelihood",
       "loglikelihood_check", "numVotes", "numVotes_check",
       "numErrors", "numErrors_check", "GMP", "GMP_check")
-  legs = read.fwf("legislator_output.dat", widths = legwidths,
-      col.names = legnames)
+  format1 = c("I4", "I6", "I3", "I2", "X", "A7", "X",
+      "I4", "X", "A11", "6F7", "2F12", "4I5", "2F7")
+  legs = read.fortran("legislator_output.dat", format1,
+                      col.names = legnames, na.strings = nas)
 
   # get rid of first 3 iterations
   legs = legs[(3 * nrow(legs) / 4 + 1):nrow(legs), ]
-  if (class(legs$var2D) == "factor")
-    legs$var2D = as.numeric(as.character(legs$var2D))
 
   legs$party = names(party_dict)[legs$partyID]
 
-  rcwidths = c(3, 5, 7, 7, 7, 7)
   rcnames = c("session", "ID", "midpoint1D", "spread1D",
       "midpoint2D", "spread2D")
-  rcs = read.fwf("rollcall_output.dat", widths = rcwidths,
-      col.names = rcnames)
+  format2 = c("I3", "I5", "4F7")
+  rcs = read.fortran("rollcall_output.dat", format2,
+      col.names = rcnames, na.strings = nas)
 
   # get rid of early iterations
   rcs = rcs[(3 * nrow(rcs) / 4 + 1):nrow(rcs), ]
 
   nas = which(rcs$spread1D == 0 & rcs$midpoint1D == 0 &
                 rcs$spread2D == 0 & rcs$midpoint2D == 0)
-  rcs$midpoint1D[nas] = NA
-  rcs$spread1D[nas] = NA
-  rcs$midpoint2D[nas] = NA
-  rcs$spread2D[nas] = NA
+  rcs[nas, 3:6] = NA
 
   list(legislators = legs, rollcalls = rcs)
 }
@@ -211,7 +229,7 @@ dwnominate = function(rc_list, wnom_list = NA) {
   
   for (n in 1:length(rc_list)) {
     rc_list[[n]]$legis.data$party =
-      party_dict[rc_list[[n]]$legis.data$party]
+      party_dict[as.character(rc_list[[n]]$legis.data$party)]
   }
   if (is.na(wnom_list)[1]) {
     cat("Running W-NOMINATE to get starting values...")
@@ -219,6 +237,8 @@ dwnominate = function(rc_list, wnom_list = NA) {
         function(x) wnominate(x, polarity = 1:2))
   }
   for (n in 2:length(rc_list)) {
+    id = ifelse("icpsrLegis" %in% names(rc_list[[n]]$legis.data),
+                "icpsrLegis", "ID")
     # trying to keep all W-NOMINATE results oriented in the same
     # direction
     rc0 = rc_list[[n - 1]]
@@ -226,27 +246,29 @@ dwnominate = function(rc_list, wnom_list = NA) {
     wnom0 = wnom_list[[n - 1]]
     wnom = wnom_list[[n]]
     # 1st dimension
-    low_ids = rc0$legis.data$ID[wnom0$legislators$coord1D < 0]
-    high_ids = rc0$legis.data$ID[wnom0$legislators$coord1D > 0]
+    low_ids = rc0$legis.data[wnom0$legislators$coord1D < 0, id]
+    high_ids = rc0$legis.data[wnom0$legislators$coord1D > 0, id]
     mean_low = mean(
-        wnom$legislators$coord1D[rc$legis.data$ID %in%
+        wnom$legislators$coord1D[rc$legis.data[, id] %in%
                                  low_ids], na.rm = T)
     mean_high = mean(
-        wnom$legislators$coord1D[rc$legis.data$ID %in%
+        wnom$legislators$coord1D[rc$legis.data[, id] %in%
                                  high_ids], na.rm = T)
     if (mean_high < mean_low)
-      wnom$legislators$coord1D = -wnom$legislators$coord1D
+      wnom_list[[n]]$legislators$coord1D =
+        -wnom_list[[n]]$legislators$coord1D
     # second dimension
-    low_ids = rc0$legis.data$ID[wnom0$legislators$coord2D < 0]
-    high_ids = rc0$legis.data$ID[wnom0$legislators$coord2D > 0]
+    low_ids = rc0$legis.data[wnom0$legislators$coord2D < 0, id]
+    high_ids = rc0$legis.data[wnom0$legislators$coord2D > 0, id]
     mean_low = mean(
-        wnom$legislators$coord2D[rc$legis.data$ID %in%
+        wnom$legislators$coord2D[rc$legis.data[, id] %in%
                                  low_ids], na.rm = T)
     mean_high = mean(
-        wnom$legislators$coord2D[rc$legis.data$ID %in%
+        wnom$legislators$coord2D[rc$legis.data[, id] %in%
                                  high_ids], na.rm = T)
     if (mean_high < mean_low)
-      wnom$legislators$coord2D = -wnom$legislators$coord2D
+      wnom_list[[n]]$legislators$coord2D =
+        -wnom_list[[n]]$legislators$coord2D
   }
   
   write_input_files(rc_list, wnom_list)
@@ -258,11 +280,13 @@ dwnominate = function(rc_list, wnom_list = NA) {
   .Fortran("dwnominate", startfile = nomstart)
   runtime = Sys.time() - start
   units(runtime) = "mins"
-  cat(paste("DW-NOMINATE took", round(runtime, 2),
+  cat(paste("DW-NOMINATE took", round(runtime, 1),
             "minutes.\n"))
   
   # get the results
   results = read_output_files(party_dict)
+  results$wnom_list = wnom_list
+  results$rc_list = rc_list
   class(results) = "dwnominate"
   results
 }
@@ -274,14 +298,16 @@ plot.dwnominate = function(dw_results) {
   col_dict = setNames(rainbow(length(uniq_parties)),
       uniq_parties)
   ts1 = aggregate(legs$coord1D,
-      by = list(legs$session, legs$party), FUN = mean)
+      by = list(legs$session, legs$party), FUN = mean, na.rm = T)
   plot(ts1$Group.1, ts1$x, ylim = c(-1, 1), type = "n",
-       ylab = "Mean Party Position", xlab = "Session")
-  grid(NA, ny = NULL, col = "#666666")
+       main = "DW-NOMINATE 1st Dimension",
+       ylab = "Party Means", xlab = "Session")
+  grid(NA, NULL, col = "#666666")
   for (party in unique(ts1$Group.2)) {
     ts2 = ts1[ts1$Group.2 == party, ]
     points(ts2$Group.1, ts2$x, col = col_dict[party],
            type = "o", pch = pch)
   }
-  legend("topleft", names(col_dict), fill = col_dict, bg = "white")
+  legend("topleft", names(col_dict), fill = col_dict,
+         bg = "white", horiz = T)
 }
