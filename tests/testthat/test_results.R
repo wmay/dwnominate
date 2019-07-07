d84bfdc24b5963352dd640f0ec61ffee2d0e83ee
William May <williamcmay@live.com>
2019-07-07 14:49:19 -0400

    Allow slight differences in dwnominate test results
    The exact results appear to be dependent on the specific compiler
    used.

--- tests/testthat/test_results.R
+++ tests/testthat/test_results.R
@@ -3,10 +3,10 @@ context("DW-NOMINATE results")
 data(senate)
 load('senate_results.rda')
 
-test_that("DW-NOMINATE results matches known correct results", {
+test_that("DW-NOMINATE results closely correlate with known correct results", {
   r2 = dwnominate(senate, start=r1$start)
-  max1D_diff = max(abs(r2$legislators$coord1D - r1$legislators$coord1D))
-  max2D_diff = max(abs(r2$legislators$coord2D - r1$legislators$coord2D))
-  expect_equal(max1D_diff, 0)
-  expect_equal(max2D_diff, 0)
+  cor1D = cor(r1$legislators$coord1D, r2$legislators$coord1D)
+  cor2D = cor(r1$legislators$coord2D, r2$legislators$coord2D)
+  expect_gt(cor1D, .999)
+  expect_gt(cor2D, .95)
 })
