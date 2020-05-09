7690c64bf01823d75571491e470f10da6b344fd2
William May <williamcmay@live.com>
2020-05-09 13:33:46 -0400

    Add New Hampshire Senate dataset and update documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' New Hampshire Senate Roll Calls
+#'
+#' New Hampshire Senate roll call votes from 1999 to 2019.
+#'
+#' @format A list of \code{\link[pscl]{rollcall}} objects from the pscl package
+#'
+#' @source \href{http://gencourt.state.nh.us}{New Hampshire General Court}
+#'
+#' @examples
+#' library(pscl)
+#' data(nhsen)
+#' summary(nhsen[[1]])
+'nhsen'
