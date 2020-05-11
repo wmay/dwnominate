cade6703a6d2406e34c50320ca894d52bed8c6b9
William May <williamcmay@live.com>
2020-05-10 22:08:57 -0400

    Fix unit test

--- tests/testthat/test_results.R
+++ tests/testthat/test_results.R
@@ -4,7 +4,7 @@ data(senate)
 load('senate_results.rda')
 
 test_that("DW-NOMINATE results closely correlate with known correct results", {
-  r2 = dwnominate(senate, start=r1$start)
+  r2 = dwnominate(senate, id = 'icpsrLegis', start=r1$start)
   cor1D = cor(r1$legislators$coord1D, r2$legislators$coord1D)
   cor2D = cor(r1$legislators$coord2D, r2$legislators$coord2D)
   expect_gt(cor1D, .999)
