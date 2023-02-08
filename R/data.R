af9d34f2be309eb9e778f41b614385855824b873
William May <wmay@hey.com>
2023-02-07 21:42:25 -0500

    Fix usage documentation for nhsenate

--- R/data.R
+++ R/data.R
@@ -2,6 +2,8 @@
 #'
 #' New Hampshire Senate roll call votes from 1999 to 2019.
 #'
+#' @usage data(nhsenate)
+#'
 #' @format A list of \code{\link[pscl]{rollcall}} objects from the pscl package
 #'
 #' @source \href{http://gencourt.state.nh.us}{New Hampshire General Court}
